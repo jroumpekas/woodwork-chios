a7b1f8ce270522228460c26bcaebfa09c93a1167
Dim.Roumpekas <jimroumpi@gmail.com>
2026-05-21 14:56:00 +0300

    feat(data): add works catalog with images and descriptions

---
+++ src/data/work.ts
@@ -0,0 +1,88 @@
+import work1 from "../assets/works/work1.jpg";
+import work2 from "../assets/works/work2.jpg";
+import work3 from "../assets/works/work3.jpg";
+import work4 from "../assets/works/work4.jpg";
+import kitchen1 from "../assets/works/kitchen_1/image-1.jpg"
+import kitchen2 from "../assets/works/kitchen_1/image-2.jpg"
+import kitchen3 from "../assets/works/kitchen_1/image-3.jpg"
+import kitchen4 from "../assets/works/kitchen_1/image-4.jpg"
+import kitchen5 from "../assets/works/kitchen_1/image-5.jpg"
+import kitchen6 from "../assets/works/kitchen_1/image-6.jpg"
+import kitchen7 from "../assets/works/kitchen_1/image-7.jpg"
+import kitchen8 from "../assets/works/kitchen_1/image-8.jpg"
+import kitchen9 from "../assets/works/kitchen_1/image-9.jpg"
+import kitchen10 from "../assets/works/kitchen_1/image-10.jpg"
+import kitchen11 from "../assets/works/kitchen_1/image-11.jpg"
+import kitchen12 from "../assets/works/kitchen_1/image-12.jpg"
+import kitchen13 from "../assets/works/kitchen_1/image-13.jpg"
+import kitchen14 from "../assets/works/kitchen_1/image-14.jpg"
+
+import wood2_1 from "../assets/works/kitchen_2/image-1.jpg"
+import wood2_2 from "../assets/works/kitchen_2/image-2.jpg"
+import wood2_3 from "../assets/works/kitchen_2/image-3.jpg"
+import wood2_4 from "../assets/works/kitchen_2/image-4.jpg"
+import wood2_5 from "../assets/works/kitchen_2/image-5.jpg"
+
+import sample1 from "../assets/works/kitchen_3/image-1.jpg"
+import sample2 from "../assets/works/kitchen_3/image-2.jpg"
+import sample3 from "../assets/works/kitchen_3/image-3.jpg"
+import sample4 from "../assets/works/kitchen_3/image-4.jpg"
+import sample5 from "../assets/works/kitchen_3/image-5.jpg"
+import sample6 from "../assets/works/kitchen_3/image-6.jpg"
+import sample7 from "../assets/works/kitchen_3/image-7.jpg"
+import sample8 from "../assets/works/kitchen_3/image-8.jpg"
+import sample9 from "../assets/works/kitchen_3/image-9.jpg"
+
+import office1 from "../assets/works/kitchen_4/image-1.jpg"
+import office2 from "../assets/works/kitchen_4/image-2.jpg"
+import office3 from "../assets/works/kitchen_4/image-3.jpg"
+import office4 from "../assets/works/kitchen_4/image-4.jpg"
+import office5 from "../assets/works/kitchen_4/image-5.jpg"
+import office6 from "../assets/works/kitchen_4/image-7.jpg"
+import office7 from "../assets/works/kitchen_4/image-8.jpg"
+import office8 from "../assets/works/kitchen_4/image-9.jpg"
+import office9 from "../assets/works/kitchen_4/image-10.jpg"
+import office10 from "../assets/works/kitchen_4/image-11.jpg"
+import office11 from "../assets/works/kitchen_4/image-12.jpg"
+import office12 from "../assets/works/kitchen_4/image-13.jpg"
+import office13 from "../assets/works/kitchen_4/image-14.jpg"
+import office14 from "../assets/works/kitchen_4/image-15.jpg"
+
+
+
+
+export const works = [
+    {
+        slug: "xylini-kataskevi",
+        title: "Ξύλινη κατασκευή",
+        image: work1,
+        description:
+            "Ενδεικτική ξύλινη κατασκευή με έμφαση στη λεπτομέρεια, την ποιότητα των υλικών και την πρακτικότητα του χώρου.",
+        images: [kitchen1, kitchen2, kitchen3,kitchen4,kitchen5,kitchen6,kitchen7,kitchen8,kitchen9,kitchen10,kitchen11,kitchen12,
+            kitchen13,kitchen14],
+    },
+    {
+        slug: "epiplo-kata-paraggelia",
+        title: "Έπιπλο κατά παραγγελία",
+        image: work2,
+        description:
+            "Κατασκευή επίπλου προσαρμοσμένη στις ανάγκες του πελάτη και στις διαστάσεις του χώρου.",
+        images: [wood2_1, wood2_2, wood2_3, wood2_4,wood2_5],
+    },
+    {
+        slug: "esoteriki-xylourgiki-ergasia",
+        title: "Εσωτερική ξυλουργική εργασία",
+        image: work3,
+        description:
+            "Εσωτερικές ξυλουργικές εργασίες με στόχο την αισθητική, τη λειτουργικότητα και την αντοχή στον χρόνο.",
+        images: [sample1, sample2, sample3, sample4, sample5, sample6, sample7, sample8, sample9],
+    },
+    {
+        slug: "eidiki-xylini-kataskevi",
+        title: "Ειδική ξύλινη κατασκευή",
+        image: work4,
+        description:
+            "Ειδική ξύλινη κατασκευή κατά παραγγελία για κατοικίες ή επαγγελματικούς χώρους.",
+        images: [office1, office2,office3,office4,office5,office6,office7,office8, office9, office10, office11, office12, office13, office14],
+    },
+];
