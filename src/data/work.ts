4a8697d2410ef61be7f7ecbf2fbf83663ab93da4
Dim.Roumpekas <jimroumpi@gmail.com>
2026-05-29 17:03:07 +0300

    refactor(data): organize portfolio work data

--- src/data/work.ts
+++ src/data/work.ts
@@ -2,64 +2,125 @@ import work1 from "../assets/works/work1.jpg";
 import work2 from "../assets/works/work2.jpg";
 import work3 from "../assets/works/work3.jpg";
 import work4 from "../assets/works/work4.jpg";
-import kitchen1 from "../assets/works/kitchen_1/image-1.jpg"
-import kitchen2 from "../assets/works/kitchen_1/image-2.jpg"
-import kitchen3 from "../assets/works/kitchen_1/image-3.jpg"
-import kitchen4 from "../assets/works/kitchen_1/image-4.jpg"
-import kitchen5 from "../assets/works/kitchen_1/image-5.jpg"
-import kitchen6 from "../assets/works/kitchen_1/image-6.jpg"
-import kitchen7 from "../assets/works/kitchen_1/image-7.jpg"
-import kitchen8 from "../assets/works/kitchen_1/image-8.jpg"
-import kitchen9 from "../assets/works/kitchen_1/image-9.jpg"
-import kitchen10 from "../assets/works/kitchen_1/image-10.jpg"
-import kitchen11 from "../assets/works/kitchen_1/image-11.jpg"
-import kitchen12 from "../assets/works/kitchen_1/image-12.jpg"
-import kitchen13 from "../assets/works/kitchen_1/image-13.jpg"
-import kitchen14 from "../assets/works/kitchen_1/image-14.jpg"
 
-import wood2_1 from "../assets/works/kitchen_2/image-1.jpg"
-import wood2_2 from "../assets/works/kitchen_2/image-2.jpg"
-import wood2_3 from "../assets/works/kitchen_2/image-3.jpg"
-import wood2_4 from "../assets/works/kitchen_2/image-4.jpg"
-import wood2_5 from "../assets/works/kitchen_2/image-5.jpg"
+import kitchen1 from "../assets/works/kitchen_1/image-1.jpg";
+import kitchen2 from "../assets/works/kitchen_1/image-2.jpg";
+import kitchen3 from "../assets/works/kitchen_1/image-3.jpg";
+import kitchen4 from "../assets/works/kitchen_1/image-4.jpg";
+import kitchen5 from "../assets/works/kitchen_1/image-5.jpg";
+import kitchen6 from "../assets/works/kitchen_1/image-6.jpg";
+import kitchen7 from "../assets/works/kitchen_1/image-7.jpg";
+import kitchen8 from "../assets/works/kitchen_1/image-8.jpg";
+import kitchen9 from "../assets/works/kitchen_1/image-9.jpg";
+import kitchen10 from "../assets/works/kitchen_1/image-10.jpg";
+import kitchen11 from "../assets/works/kitchen_1/image-11.jpg";
+import kitchen12 from "../assets/works/kitchen_1/image-12.jpg";
+import kitchen13 from "../assets/works/kitchen_1/image-13.jpg";
+import kitchen14 from "../assets/works/kitchen_1/image-14.jpg";
 
-import sample1 from "../assets/works/kitchen_3/image-1.jpg"
-import sample2 from "../assets/works/kitchen_3/image-2.jpg"
-import sample3 from "../assets/works/kitchen_3/image-3.jpg"
-import sample4 from "../assets/works/kitchen_3/image-4.jpg"
-import sample5 from "../assets/works/kitchen_3/image-5.jpg"
-import sample6 from "../assets/works/kitchen_3/image-6.jpg"
-import sample7 from "../assets/works/kitchen_3/image-7.jpg"
-import sample8 from "../assets/works/kitchen_3/image-8.jpg"
-import sample9 from "../assets/works/kitchen_3/image-9.jpg"
+import wood2_1 from "../assets/works/kitchen_2/image-1.jpg";
+import wood2_2 from "../assets/works/kitchen_2/image-2.jpg";
+import wood2_3 from "../assets/works/kitchen_2/image-3.jpg";
+import wood2_4 from "../assets/works/kitchen_2/image-4.jpg";
+import wood2_5 from "../assets/works/kitchen_2/image-5.jpg";
 
-import office1 from "../assets/works/kitchen_4/image-1.jpg"
-import office2 from "../assets/works/kitchen_4/image-2.jpg"
-import office3 from "../assets/works/kitchen_4/image-3.jpg"
-import office4 from "../assets/works/kitchen_4/image-4.jpg"
-import office5 from "../assets/works/kitchen_4/image-5.jpg"
-import office6 from "../assets/works/kitchen_4/image-7.jpg"
-import office7 from "../assets/works/kitchen_4/image-8.jpg"
-import office8 from "../assets/works/kitchen_4/image-9.jpg"
-import office9 from "../assets/works/kitchen_4/image-10.jpg"
-import office10 from "../assets/works/kitchen_4/image-11.jpg"
-import office11 from "../assets/works/kitchen_4/image-12.jpg"
-import office12 from "../assets/works/kitchen_4/image-13.jpg"
-import office13 from "../assets/works/kitchen_4/image-14.jpg"
-import office14 from "../assets/works/kitchen_4/image-15.jpg"
+import sample1 from "../assets/works/kitchen_3/image-1.jpg";
+import sample2 from "../assets/works/kitchen_3/image-2.jpg";
+import sample3 from "../assets/works/kitchen_3/image-3.jpg";
+import sample4 from "../assets/works/kitchen_3/image-4.jpg";
+import sample5 from "../assets/works/kitchen_3/image-5.jpg";
+import sample6 from "../assets/works/kitchen_3/image-6.jpg";
+import sample7 from "../assets/works/kitchen_3/image-7.jpg";
+import sample8 from "../assets/works/kitchen_3/image-8.jpg";
+import sample9 from "../assets/works/kitchen_3/image-9.jpg";
 
+import office1 from "../assets/works/kitchen_4/image-1.jpg";
+import office2 from "../assets/works/kitchen_4/image-2.jpg";
+import office3 from "../assets/works/kitchen_4/image-3.jpg";
+import office4 from "../assets/works/kitchen_4/image-4.jpg";
+import office5 from "../assets/works/kitchen_4/image-5.jpg";
+import office6 from "../assets/works/kitchen_4/image-7.jpg";
+import office7 from "../assets/works/kitchen_4/image-8.jpg";
+import office8 from "../assets/works/kitchen_4/image-9.jpg";
+import office9 from "../assets/works/kitchen_4/image-10.jpg";
+import office10 from "../assets/works/kitchen_4/image-11.jpg";
+import office11 from "../assets/works/kitchen_4/image-12.jpg";
+import office12 from "../assets/works/kitchen_4/image-13.jpg";
+import office13 from "../assets/works/kitchen_4/image-14.jpg";
+import office14 from "../assets/works/kitchen_4/image-15.jpg";
 
+export type Work = {
+    slug: string;
+    title: string;
+    image: string;
+    description: string;
+    images: string[];
+    featured?: boolean;
+};
 
+const kitchenImages = [
+    kitchen1,
+    kitchen2,
+    kitchen3,
+    kitchen4,
+    kitchen5,
+    kitchen6,
+    kitchen7,
+    kitchen8,
+    kitchen9,
+    kitchen10,
+    kitchen11,
+    kitchen12,
+    kitchen13,
+    kitchen14,
+];
 
-export const works = [
+const customFurnitureImages = [
+    wood2_1,
+    wood2_2,
+    wood2_3,
+    wood2_4,
+    wood2_5,
+];
+
+const interiorWoodworkImages = [
+    sample1,
+    sample2,
+    sample3,
+    sample4,
+    sample5,
+    sample6,
+    sample7,
+    sample8,
+    sample9,
+];
+
+const specialConstructionImages = [
+    office1,
+    office2,
+    office3,
+    office4,
+    office5,
+    office6,
+    office7,
+    office8,
+    office9,
+    office10,
+    office11,
+    office12,
+    office13,
+    office14,
+];
+
+export const works: Work[] = [
     {
         slug: "xylini-kataskevi",
         title: "Ξύλινη κατασκευή",
         image: work1,
         description:
             "Ενδεικτική ξύλινη κατασκευή με έμφαση στη λεπτομέρεια, την ποιότητα των υλικών και την πρακτικότητα του χώρου.",
-        images: [kitchen1, kitchen2, kitchen3,kitchen4,kitchen5,kitchen6,kitchen7,kitchen8,kitchen9,kitchen10,kitchen11,kitchen12,
-            kitchen13,kitchen14],
+        images: kitchenImages,
+        featured: true,
     },
     {
         slug: "epiplo-kata-paraggelia",
@@ -67,7 +128,8 @@ export const works = [
         image: work2,
         description:
             "Κατασκευή επίπλου προσαρμοσμένη στις ανάγκες του πελάτη και στις διαστάσεις του χώρου.",
-        images: [wood2_1, wood2_2, wood2_3, wood2_4,wood2_5],
+        images: customFurnitureImages,
+        featured: true,
     },
     {
         slug: "esoteriki-xylourgiki-ergasia",
@@ -75,7 +137,8 @@ export const works = [
         image: work3,
         description:
             "Εσωτερικές ξυλουργικές εργασίες με στόχο την αισθητική, τη λειτουργικότητα και την αντοχή στον χρόνο.",
-        images: [sample1, sample2, sample3, sample4, sample5, sample6, sample7, sample8, sample9],
+        images: interiorWoodworkImages,
+        featured: true,
     },
     {
         slug: "eidiki-xylini-kataskevi",
@@ -83,6 +146,13 @@ export const works = [
         image: work4,
         description:
             "Ειδική ξύλινη κατασκευή κατά παραγγελία για κατοικίες ή επαγγελματικούς χώρους.",
-        images: [office1, office2,office3,office4,office5,office6,office7,office8, office9, office10, office11, office12, office13, office14],
+        images: specialConstructionImages,
+        featured: true,
     },
 ];
+
+export const featuredWorks = works.filter((work) => work.featured);
+
+export const getWorkBySlug = (slug: string | undefined) => {
+    return works.find((work) => work.slug === slug);
+};
